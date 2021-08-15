6095d8f61ea4b9c80bf0967da8d2a0356297fa16
Maximilian Deister <maxikd@hotmail.com>
2021-08-15 16:34:20 -0300

    Adding iteration

---
+++ Max_codechallenge/Iteration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.Iterate();
+        }
+    }
+}
---
+++ Max_codechallenge/Iteration/QuestionClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iteration
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>() {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+        public static void Iterate()
+        {
+            var enumerator = NamesList.GetEnumerator();
+
+            while (enumerator.MoveNext())
+                Console.WriteLine(enumerator.Current);
+        }
+    }
+}
